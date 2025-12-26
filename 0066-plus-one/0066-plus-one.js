448fcbc3eb6456ee4eb0f88c1992d71101ef8e2c
KothaiSundar <131440849+KothaiSundar@users.noreply.github.com>
2025-12-26 14:13:44 +0100

    Time: 0 ms (100%), Space: 54.1 MB (51.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,21 +3,20 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-   var i=digits.length-1;
-    
-    while(digits[i]===9)
+    let n=digits.length;
+
+    for(let i=n-1;i>=0;i--)
+    {
+        if(digits[i]<9)
         {
-            digits[i]=0;
-            i--;
+            digits[i]++;
+            return digits;
         }
-    if(i<0)
-        {
-            var array=[1];
-            digits=array.concat(digits);
-        }
-    else{
-        digits[i]++;
+        digits[i]=0;
     }
-    
-    return digits;
+
+let result = new Array(n + 1).fill(0);   
+ result[0]=1;
+
+    return result;
 };
